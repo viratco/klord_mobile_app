2c88c87e4a5acf25c59bbde8e2fb04acd26ea427
viratco <viratvaroniya31@gmail.com>
2026-01-13 12:12:26 +0530

    Fix: Exclude org.jfrog.buildinfo transitive dependency from classpath

--- plugins/withRootRepos.js
+++ plugins/withRootRepos.js
@@ -3,18 +3,33 @@ const { withProjectBuildGradle } = require('expo/config-plugins');
 module.exports = function withRootRepos(config) {
     return withProjectBuildGradle(config, (config) => {
         if (config.modResults.language === 'groovy') {
-            const buildGradle = config.modResults.contents;
-            // Look for the buildscript repositories block
+            let buildGradle = config.modResults.contents;
+
+            // Add JFrog releases repository if not present
             const pattern = /buildscript\s*\{[\s\S]*?repositories\s*\{/;
-
-            // If it exists and we haven't added the repo yet
             if (pattern.test(buildGradle) && !buildGradle.includes('releases.jfrog.io')) {
-                const replacement = `buildscript {
+                buildGradle = buildGradle.replace(pattern, `buildscript {
     repositories {
-        maven { url 'https://releases.jfrog.io/artifactory/oss-releases' }`;
-
-                config.modResults.contents = buildGradle.replace(pattern, replacement);
+        maven { url 'https://releases.jfrog.io/artifactory/oss-releases' }`);
             }
+
+            // Add exclusion for the problematic JFrog buildinfo dependency
+            if (!buildGradle.includes('exclude group: \'org.jfrog.buildinfo\'')) {
+                const exclusionCode = `
+configurations.classpath {
+    exclude group: 'org.jfrog.buildinfo', module: 'build-info-extractor-gradle'
+}
+`;
+                // Find the end of buildscript block dependencies and add exclusion after
+                const depsEndPattern = /(buildscript\s*\{[\s\S]*?dependencies\s*\{[\s\S]*?\}\s*\n)/;
+                if (depsEndPattern.test(buildGradle)) {
+                    buildGradle = buildGradle.replace(depsEndPattern, (match) => {
+                        return match + exclusionCode;
+                    });
+                }
+            }
+
+            config.modResults.contents = buildGradle;
         }
         return config;
     });
