7b0814d0b5aadc86d768c2f59be046c5c0128726
viratco <viratvaroniya31@gmail.com>
2026-01-13 14:02:12 +0530

    Fix: Revert aggressive build fixes (awaiting Expo service recovery)

--- plugins/withRootRepos.js
+++ plugins/withRootRepos.js
@@ -3,33 +3,18 @@ const { withProjectBuildGradle } = require('expo/config-plugins');
 module.exports = function withRootRepos(config) {
     return withProjectBuildGradle(config, (config) => {
         if (config.modResults.language === 'groovy') {
-            let buildGradle = config.modResults.contents;
-
-            // Add JFrog releases repository if not present
+            const buildGradle = config.modResults.contents;
+            // Look for the buildscript repositories block
             const pattern = /buildscript\s*\{[\s\S]*?repositories\s*\{/;
+
+            // If it exists and we haven't added the repo yet
             if (pattern.test(buildGradle) && !buildGradle.includes('releases.jfrog.io')) {
-                buildGradle = buildGradle.replace(pattern, `buildscript {
+                const replacement = `buildscript {
     repositories {
-        maven { url 'https://releases.jfrog.io/artifactory/oss-releases' }`);
-            }
+        maven { url 'https://releases.jfrog.io/artifactory/oss-releases' }`;
 
-            // Add exclusion for the problematic JFrog buildinfo dependency
-            if (!buildGradle.includes('exclude group: \'org.jfrog.buildinfo\'')) {
-                const exclusionCode = `
-configurations.classpath {
-    exclude group: 'org.jfrog.buildinfo', module: 'build-info-extractor-gradle'
-}
-`;
-                // Find the end of buildscript block dependencies and add exclusion after
-                const depsEndPattern = /(buildscript\s*\{[\s\S]*?dependencies\s*\{[\s\S]*?\}\s*\n)/;
-                if (depsEndPattern.test(buildGradle)) {
-                    buildGradle = buildGradle.replace(depsEndPattern, (match) => {
-                        return match + exclusionCode;
-                    });
-                }
+                config.modResults.contents = buildGradle.replace(pattern, replacement);
             }
-
-            config.modResults.contents = buildGradle;
         }
         return config;
     });
